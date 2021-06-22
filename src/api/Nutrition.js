c566f3105c4ce6f24f88bb0f677c4df0a6437e9c
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2021-06-21 22:20:23 -0700

    Add api

---
+++ src/api/Nutrition.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Nutrition extends React.Component {
+
+  componentDidMount: function() {
+    method: 'GET',
+    url: 'https://api.calorieninjas.com/v1/nutrition?query=' + query,
+    headers: { 'X-Api-Key': 'YOUR_API_KEY'},
+    contentType: 'application/json',
+    success: function(result) {
+        console.log(result);
+    },
+    error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
+    }
+  }
+
+
+}
