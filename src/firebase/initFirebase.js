ed9fd3676e60bc910ad0656c8b080e36302b66a9
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2021-07-18 00:50:53 -0700

    Change authDomain to custom domain name

--- src/firebase/initFirebase.js
+++ src/firebase/initFirebase.js
@@ -3,9 +3,8 @@ import 'firebase/auth';
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "...",
-  // apiKey: "AIzaSyApfm-ZbIXBn9II4XyZPHIUakcllOz_b4Q",
-  authDomain: "auth.recipe-and-calorie-manager.com",
+  apiKey: "AIzaSyApfm-ZbIXBn9II4XyZPHIUakcllOz_b4Q",
+  authDomain: "recipe-and-calorie-manager.com",
   databaseURL: "https://recipe-and-calorie-default-rtdb.firebaseio.com",
   projectId: "recipe-and-calorie",
   storageBucket: "recipe-and-calorie.appspot.com",
