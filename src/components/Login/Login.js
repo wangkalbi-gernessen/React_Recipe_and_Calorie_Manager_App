3c87ce4ab4e126d66148e94ab82a64b064a90553
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2022-02-26 13:11:34 -0800

    Add a new mixin for errorMessage margin

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -24,35 +24,35 @@ const Login = () => {
     <Grid container className="login" direction="column" alignItems="center" justify="center">
       <Grid item xs={10} sm={10} md={10} xl={10} lg={10} className="login__item">
       <Typography variant="h2" align="center"  className="login__title" style={{fontFamily: "cursive"}}>Welcome to Recipe and Calorie Manager!!</Typography>
-        <Paper elevation={3} style={{margin: "30px"}}>
-          <Typography variant="h4" align="center" gutterBottom="true" style={{fontFamily: "monospace", padding: "15px"}}>Log in here</Typography>
-          <form noValidate autoComplete="off" style={{padding: "10px", textAlign: "center"}} onSubmit={handleSubmit}>
+        <Paper elevation={3} className="login__paper">
+          <Typography variant="h4" align="center" gutterBottom="true" className="login__here" style={{fontFamily: "monospace"}}>Log in here</Typography>
+          <form noValidate autoComplete="off" className="login__form" onSubmit={handleSubmit}>
             <Grid container>
               <Grid item xs={12} sm={12} md={12} xl={12} lg={12}>
                 {errorMessage !== null && (
-                  <Typography variant="h6" color="secondary" style={{marginBottom:"15px"}}>{errorMessage}</Typography>
+                  <Typography variant="h6" color="secondary" className="login__form_errorMessage" >{errorMessage}</Typography>
                 )}
               </Grid>
-              <Grid item xs={12} sm={12} md={12} xl={12} lg={12} style={{paddingTop:"10px"}}>
+              <Grid item xs={12} sm={12} md={12} xl={12} lg={12} className="login__form_gridItem">
                 <TextField id="my-email" type="email" label="Email Address" variant="outlined" onChange={(e) => setForm({...form, email: e.target.value})}/>
               </Grid>
-              <Grid item xs={12} sm={12} md={12} xl={12} lg={12} style={{paddingTop:"10px"}}>
+              <Grid item xs={12} sm={12} md={12} xl={12} lg={12} className="login__form_gridItem">
                 <TextField id="my-password" type="password" label="Password" variant="outlined" onChange={(e) => setForm({...form, password: e.target.value})} />
               </Grid>
-              <Grid item xs={12} sm={12} md={12} xl={12} lg={12} style={{paddingTop:"10px"}}>
-                <Button type="submit" variant="contained" size="medium" color="primary" style={{cursor: "pointer"}} >Log in</Button>
+              <Grid item xs={12} sm={12} md={12} xl={12} lg={12} className="login__form_gridItem">
+                <Button type="submit" variant="contained" size="medium" color="primary" className="login__form_loginBtn">Log in</Button>
               </Grid>
             </Grid>
           </form>
-          <Container style={{textAlign:"center", marginTop: "10px"}}>
-            <Typography align="center" style={{fontWeight: "bold"}}>Don't have an account?</Typography>
-            <Container style={{width: "100%", margin: "auto", textAlign: "center", padding: "10px"}}>
-              <Link to="/SignUp/SignUp" style={{textDecoration: "none", color: "white"}}>
+          <Container className="login__noAccount">
+            <Typography align="center" className="login__noAccount_text">Don't have an account?</Typography>
+            <Container className="login__noAccount_btnContainer">
+              <Link to="/SignUp/SignUp" className="login__noAccount_btnLink">
                 <Button variant="contained" size="medium" color="primary">Sign up here</Button>
               </Link>
             </Container>
-            <Container style={{width: "100%", margin: "auto", textAlign: "center", padding: "10px"}}>
-              <Link to="/PasswordReset/PasswordReset" style={{textDecoration: "none", color: "white"}}>
+            <Container className="login__passwordReset">
+              <Link to="/PasswordReset/PasswordReset" className="login__passwordReset_link">
                 <Button variant="contained" size="medium" color="secondary">Forgot Password?</Button>
               </Link>
             </Container>
