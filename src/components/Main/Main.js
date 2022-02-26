809efcd5cf1bc8f826b23b145952cc2107880721
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2022-02-25 22:32:41 -0800

    Add scss files for Main and RecipeList

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/alt-text */
-import { Radio, Container, FormControl, FormControlLabel, Grid, makeStyles, TextField, Button, Typography, RadioGroup } from "@material-ui/core";
+import { Radio, Container, FormControl, FormControlLabel, Grid, TextField, Button, Typography, RadioGroup } from "@material-ui/core";
 import { auth } from "../../firebase/initFirebase";
 import friedEgg from "../../img/Main/fried-egg.png";
 import sandwich from "../../img/Main/sandwich.png";
@@ -8,28 +8,9 @@ import steak from "../../img/Main/steak.png";
 import { useState } from "react";
 import { Switch, Route, useHistory } from "react-router-dom";
 import AddIngredients from './AddIngredients';
-
-const useStyles = makeStyles({
-  content: {
-    background: "#f5f5dc",
-    minWidth: "100%",
-    minHeight: "100vh",
-    margin: 0,
-    padding: 0
-  },
-  form: {
-    width: "100%",
-    margin: "30px auto",
-    padding: 0,
-    textAlign: "center"
-  },
-  radioArea: {
-    width: "100%"
-  }
-});
+import '../../styles/mainPage.scss';
 
 const Main = () => {
-  const classes = useStyles();
   const [dishName, setDishName] = useState('');
   const [meal, setMeal] = useState('breakfast');
   const history = useHistory();
@@ -38,33 +19,33 @@ const Main = () => {
     history.push("/Main/AddIngredients", {dishName: dishName, meal: meal});
   }
   return (
-    <Container className={classes.content}>
-      <Typography align="center" variant="h4" style={{color:"#008b8b", paddingTop: "30px"}}>Add New Recipe</Typography>
-      <Typography style={{fontSize: "15px", paddingLeft: "10px", paddingTop: "30px", fontFamily: "fantasy"}}>Welcome, {auth.currentUser.email}</Typography>
-      <FormControl noValidate   autoComplete="off" className={classes.form}>
+    <Container className="main">
+      <Typography align="center" variant="h4" className="main__title">Add New Recipe</Typography>
+      <Typography className="main__emailAddress" >Welcome, {auth.currentUser.email}</Typography>
+      <FormControl noValidate   autoComplete="off" className="main__form">
         <Container >
-          <TextField  placeholder="Chicken Adobo" variant="outlined" style={{background: "white", margin: "40px 20px"}} size="large" value={dishName} onChange={(e) => setDishName(e.target.value)}/>
-          <Grid container spacing={12} justify="space-evenly" alignItems="center" className={classes.radioArea}>
+          <TextField  placeholder="Chicken Adobo" variant="outlined" className="main__form_textField" size="large" value={dishName} onChange={(e) => setDishName(e.target.value)}/>
+          <Grid container spacing={12} justify="space-evenly" alignItems="center" className="main__form_radio">
             <RadioGroup row aria-label="position" name="position" defaultValue="Breakfast" >
-              <Grid item xs={6} sm={3} md={3} lg={3} style={{textAlign: "center"}}>
+              <Grid item xs={6} sm={3} md={3} lg={3} className="main__form_img">
                 <Container>
                   <img src={friedEgg} width={60} height={60} />
                 </Container>
                 <FormControlLabel value="breakfast" control={<Radio color="primary" />}label="Breakfast" labelPlacement="top" checked={meal === 'breakfast'} onClick={() => setMeal('breakfast')} />
               </Grid>
-              <Grid item xs={6} sm={3} md={3} lg={3} style={{textAlign: "center"}}>
+              <Grid item xs={6} sm={3} md={3} lg={3} className="main__form_img">
                 <Container>
                   <img src={sandwich} width={60} height={60} />
                 </Container>
-                <FormControlLabel value="lunch" control={<Radio color="primary" />}label="Lunch"labelPlacement="top" checked={meal === 'lunch'} onClick={() => setMeal('lunch')}/>
+                <FormControlLabel value="lunch" control={<Radio color="primary" />}label="Lunch" labelPlacement="top" checked={meal === 'lunch'} onClick={() => setMeal('lunch')}/>
               </Grid>
-              <Grid item xs={6} sm={3} md={3} lg={3} style={{textAlign: "center"}}>
+              <Grid item xs={6} sm={3} md={3} lg={3} className="main__form_img">
                 <Container>
                   <img src={food} width={60} height={60} />
                 </Container>
                 <FormControlLabel value="snack" control={<Radio color="primary" />}label="Snack"labelPlacement="top" checked={meal === 'snack'} onClick={() => setMeal('snack')}/>
               </Grid>
-              <Grid item xs={6} sm={3} md={3} lg={3} style={{textAlign: "center"}}>
+              <Grid item xs={6} sm={3} md={3} lg={3} className="main__form_img">
                 <Container>
                   <img src={steak} width={60} height={60} />
                 </Container>
@@ -72,7 +53,7 @@ const Main = () => {
               </Grid>
             </RadioGroup>
           </Grid>
-          <Button size="large" variant="contained" color="secondary" style={{cursor: "pointer", marginTop: "50px"}} onClick={addRecipe} disabled={!dishName}>Add Recipe</Button>
+          <Button size="large" variant="contained" color="secondary" className="main__form_button" onClick={addRecipe} disabled={!dishName}>Add Recipe</Button>
         </Container>
       </FormControl>
       <Switch>
--- src/components/RecipeList/RecipeList.js
+++ src/components/RecipeList/RecipeList.js
@@ -1,24 +1,7 @@
-import { Paper, Grid, makeStyles, Typography, TableBody, TableRow, TableCell, Table } from"@material-ui/core";
+import { Paper, Grid, Typography, TableBody, TableRow, TableCell, Table } from"@material-ui/core";
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 import { useHistory } from "react-router-dom";
-
-const useStyles = makeStyles({
-  content: {
-    background: "#f5f5dc",
-    width: "100%",
-    minHeight: "100vh",
-    margin: 0,
-    padding: 0
-  },
-  listArea: {
-    background: "white",
-    width: "100%",
-    height: "100%",
-    margin: 0,
-    padding: "auto",
-    opacity: "0.9",
-  }
-});
+import '../../styles/recipelist.scss';
 
 const meals = [
   {id: 1, meal: "Breakfast", value: "breakfast"},
@@ -28,13 +11,12 @@ const meals = [
 ];
 
 const RecipeList = () => {
-  const classes = useStyles();
   const history = useHistory();
   return(
-    <Grid container spacing={0}direction="column" alignItems="center" justify="center" className={classes.content}>
+    <Grid container spacing={0}direction="column" alignItems="center" justify="center" className="recipelist">
       <Typography align="center" variant="h4" style={{paddingBottom: "30px"}}>Registered Recipe List</Typography>
       <Grid item xs={11}>
-        <Paper elevation={5}>
+        <Paper elevation={5} className="recipelist__paper">
           <Table>
             <TableBody>
             { meals.map((meal) => (
